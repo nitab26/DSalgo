f161c66ec623f0367d1837000156eb18b0fa3b55
Remyap126 <remyap126@gmail.com>
2024-04-20 11:20:45 -0500

    Home page feature

---
+++ DSALGO/src/test/java/definitions/HomePage.java
@@ -0,0 +1,27 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class HomePage {
+	@Given("The user should be open the browser")
+	public void the_user_should_be_open_the_browser() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user enter the DS_Algo Portal URL")
+	public void the_user_enter_the_ds_algo_portal_url() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should landed on the DS_Algo")
+	public void the_user_should_landed_on_the_ds_algo() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+}
---
+++ DSALGO/src/test/java/feature/launch_homepage.feature
@@ -0,0 +1,6 @@
+Feature: Launch the Dsalgoportal app
+
+Scenario: Launch the app and verify the page
+	Given: The user should be open the browser
+	When:  The user enter the DS_Algo Portal URL
+	Then: The user should landed on the DS_Algo 
