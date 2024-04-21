c5a182a1d5624dedb20b14f3a25a6c096493491c
nitab26 <54184378+nitab26@users.noreply.github.com>
2024-04-20 20:02:30 -0700

    commit user registration feature file

---
+++ DSALGO/src/test/java/definitions/userRegistration.java
@@ -0,0 +1,29 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class userRegistration {
+	
+	@Given("Homepage is launched")
+	public void homepage_is_launched() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks Register button")
+	public void user_clicks_register_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("go to register page for entering details")
+	public void go_to_register_page_for_entering_details() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+}
---
+++ DSALGO/src/test/resources/Features/Register.feature
@@ -0,0 +1,10 @@
+Feature: User registration
+
+Scenario: Go to DSAlgo page
+
+Given Homepage is launched
+When User clicks Register button
+Then go to register page for entering details
+
+
+
